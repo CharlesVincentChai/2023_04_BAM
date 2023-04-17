0a22acf780765d7638e3ed2b8eb3431beca8c621
CharlesVincentChai <abwls2631@gmail.com>
2023-04-17 18:33:36 +0900

    article write시 날짜 데이터 저장

--- src/bam/Main.java
+++ src/bam/Main.java
@@ -1,6 +1,9 @@
+package bam;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import bam_util.Util;
 
 public class Main {
 	public static void main(String[] args) {
@@ -14,24 +17,28 @@ public class Main {
 			System.out.printf("명령어) ");
 			String cmd = sc.nextLine();
 			if (cmd.equals("article write")) {
+				
+				String regDate = null;
+				regDate = Util.getDateStr();
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 :");
 				String body = sc.nextLine();
 				num++;
 				System.out.printf("%d번 글이 생성되었습니다.\n", num);
-				Article article = new Article(num, title, body);
+				Article article = new Article(num, regDate ,title, body);
 				articles.add(article);
 
 			} else if (cmd.equals("exit")) {
 				break;
 			} else if (cmd.equals("article list")) {
-				System.out.println("번호	|	제목");
+				System.out.println("번호	|	제목	|	작성일	");
 				for (int i = num - 1; i >= 0; i--) {
 					int id = articles.get(i).num;
 					String title = articles.get(i).title;
+					String date = articles.get(i).regDate;
 
-					System.out.printf("%d	|	%s\n", id, title);
+					System.out.printf("%d	|	%s	|	%s	\n", id, title, date);
 				} 
 			}else if (cmd.startsWith("article detail ")) {
 				
@@ -53,6 +60,7 @@ public class Main {
 					System.out.println("일치하는 번호가 없습니다.");
 				}
 					System.out.printf("번호 : %d\n", foundArticle.num);
+					System.out.printf("작성일 : %s\n", foundArticle.regDate);
 					System.out.printf("제목 : %s\n", foundArticle.title);
 					System.out.printf("내용 : %s\n", foundArticle.body);
 					
@@ -71,12 +79,17 @@ public class Main {
 
 class Article {
 	int num;
+	String regDate;
 	String title;
 	String body;
+	
 
-	Article(int num, String title, String body) {
+	Article(int num, String regDate, String title, String body) {
+		this.regDate = regDate;
 		this.num = num;
 		this.title = title;
 		this.body = body;
 	}
 }
+
+
